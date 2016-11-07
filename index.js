7afb98834e703efe378464307e1bec37cdf6ce68
krissasantos <krissa.santos31@gmail.com>
2016-11-07 09:32:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const app = "I don't do much.";
+
+// A pure function is a function that:
+// returns output solely based on its input (the parameters, if there are any);
+// is free from any side effects.
